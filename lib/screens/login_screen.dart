99a9a4f8cee55b1e67ffddb02ecb2c3f6034c099
Milca Sanchez <sanchezmilca05@gmail.com>
2026-02-26 08:47:48 -0600

    feat(rive): animation added

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //Evita nudge o cámara frontales en móviles
+      body: SafeArea(child:
+      Column(
+        children: [
+          Expanded(child: RiveAnimation.asset('animated_login_character.riv'))
+        ],
+      )
+      ),
+    );
+  }
+}
